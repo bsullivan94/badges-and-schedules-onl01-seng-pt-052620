ac4465c4f17c7cdce14197160c39697790a10f13
brody sullivan <brodysullivan0@gmail.com>
2020-06-02 16:48:15 +0000

    Automatically backed up by Learn

---
+++ conference_badges.rb
@@ -0,0 +1,15 @@
+people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+
+def batch_badge_creator(array)
+  nuarray = []
+  array.each do |name|
+    nuarray.push("Hello, my name is #{name}.")
+  end
+  return nuarray
+end
